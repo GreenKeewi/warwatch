c163b9b181573416cfa732e47a55b97737163c84
GreenKeewi <aliononmars07@gmail.com>
2025-05-13 20:22:27 -0400

    + Theme Switcher, + Tabs, + NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className="tabs tabs-border">
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab"
+          aria-label="India And Pakistan Conflict"
+        />
+        <div className="tab-content border-base-300 bg-base-100 p-10">
+          Tab content 1
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab"
+          aria-label="Ukraine And Russia Conflict"
+          defaultChecked
+        />
+        <div className="tab-content border-base-300 bg-base-100 p-10">
+          Tab content 2
+        </div>
+      </div>
+    </div>
+  );
 }
